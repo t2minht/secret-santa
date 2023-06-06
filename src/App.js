a3f9bc80a64f26c2e6c7296e13afbd105eabc78c
anhvuknguyen <avnguyen1115@gmail.com>
2023-06-06 00:10:54 -0500

    changed text in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-            Things to be added... probably
+            Secret Santa
       </header>
     </div>
   );
