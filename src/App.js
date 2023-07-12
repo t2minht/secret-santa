3fe0fad02abd92e98e6d7abb0643b40044884070
anhvuknguyen <avnguyen1115@gmail.com>
2023-07-11 22:29:35 -0500

    Almost done, submit button for thi/database

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './styles/styles.css';
 function App() {
   return (
     <div className="App" >
-      <div className="formContainer">
-        <form>
-          <label for="fam">Family: </label>
+      <form>
+        <div className='formDiv familyDiv'>
+          <label className='boldLabel' for="fam">Family: </label>
           {/*Family Choice*/}
           <select id="fam" name="family" onChange={changeName}>
             <option className="famValue" value="" selected>Family</option>
@@ -16,15 +16,135 @@ function App() {
             <option className="famValue" value="nguyet" >Nguyet/David</option>
             <option className="famValue" value="ut" >Ut/Stephen</option>
           </select>
-          <br></br>
-          <label for="cousin">Cousin: </label>
+          <label className='boldLabel' for="cousin"> Cousin: </label>
           <select id="cousin" name="cousin">
             <option className="cousinValue" value="" selected>Cousin</option>
           </select>
-        </form>
-      </div>
+        </div>
+
+        <div className='formDiv hobbiesDiv'>
+          <label className='boldLabel' for="hobbies/interests">Hobbies/Interests: </label>
+          <textarea id="hobbies/interests" name="hobbies/interests"></textarea>
+        </div>
+
+        <div className='formDiv preferencesDiv'>
+          <label className='boldLabel'>Preferences: </label>
+          <div className="preferences">
+            <div className='checkBox'>
+            <label for='av1'>Anh-Viet</label>
+            <input type='checkbox' id='av1' value={"Anh-Viet"} name='preference'></input>
+            </div>
+            
+            <div className='checkBox'>
+            <label for='av2'>Ai-Vy</label>
+            <input type='checkbox' id='av2' value={"Ai-Vy"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='av3'>Ai-Van</label>
+            <input type='checkbox' id='av3' value={"Ai-Van"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='av4'>Anh-Vu</label>
+            <input type='checkbox' id='av4' value={"Anh-Vu"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='chris'>Chris</label>
+            <input type='checkbox' id='chris' value={"Chris"} name='preference'></input>
+            </div>
+            
+            <div className='checkBox'>
+            <label for='pktk1'>Phuong</label>
+            <input type='checkbox' id='pktk1' value={"Phuong"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='pktk2'>Khoi</label>
+            <input type='checkbox' id='pktk2' value={"Khoi"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='pktk3'>Thao</label>
+            <input type='checkbox' id='pktk3' value={"Thao"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='pktk4'>Khoa</label>
+            <input type='checkbox' id='pktk4' value={"Khoa"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='tyna'>Tyna</label>
+            <input type='checkbox' id='tyna' value={"Tyna"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='t1'>Thi</label>
+            <input type='checkbox' id='t1' value={"Thi"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='t2'>Tuong</label>
+            <input type='checkbox' id='t2' value={"Tuong"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='t3'>Thien-An</label>
+            <input type='checkbox' id='t3' value={"Thien-An"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='t4'>Thai-Hien</label>
+            <input type='checkbox' id='t4' value={"Thai-Hien"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='t5'>Thuan</label>
+            <input type='checkbox' id='t5' value={"Thuan"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='h1'>David</label>
+            <input type='checkbox' id='t1' value={"David"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='h2'>Madison</label>
+            <input type='checkbox' id='t2' value={"Madison"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='h3'>Aidan</label>
+            <input type='checkbox' id='t3' value={"Aidan"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='h4'>Brayson</label>
+            <input type='checkbox' id='h4' value={"Brayson"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='m1'>Mai-Linh</label>
+            <input type='checkbox' id='m1' value={"Mai-Linh"} name='preference'></input>
+            </div>
+
+            <div className='checkBox'>
+            <label for='m2'>Thai-Binh</label>
+            <input type='checkbox' id='m2' value={"Thai-Binh"} name='preference'></input>
+            </div>
+          </div>
+        </div>
+        <div className='submitButton '>
+          <button onClick={submit}className='submit formDiv'>Submit!</button>
+        </div>
+      </form>
     </div>
   );
+  function submit(){
+    alert("Very Good :)\n\nIf you would like to change any of your selections, resubmit the form under your name.\n\n*NOTE* This will override your entire previous submission, meaning you must resubmit items that you had previously entered!!!")
+  }
   function changeName() {
     var getOptions = document.getElementsByClassName("famValue");
     var famArr = new Array(6);
@@ -71,6 +191,8 @@ function App() {
         }
         break;
       default:
+        let newOption = new Option("Cousin","");
+        cousinsOptions.add(newOption,undefined);
         break;
     }
   }
