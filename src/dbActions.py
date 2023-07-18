98342053b57efb8894113f17f1cef53662a744ec
Thi Tran <torghiria@gmail.com>
2023-07-18 16:57:28 -0500

    final pt 1

--- src/dbActions.py
+++ src/dbActions.py
@@ -1,16 +1,21 @@
 import firebase_admin
 from firebase_admin import db
 
+
 def intialize():
-	cred_object = firebase_admin.credentials.Certificate(
-		'./cousin-secret-santa-firebase-adminsdk-pqryk-5a15348d35.json')
-	databaseURL = 'https://cousin-secret-santa-default-rtdb.firebaseio.com/'
-	default_app = firebase_admin.initialize_app(cred_object, {
-		'databaseURL': databaseURL
-	})
+    # generate JSON from Fire base -> Project Settings -> Service Accounts: Press generate private key
+    # Put JSON in src directory
+    # name it: "cousin-secret-santa-database.json"
+    cred_object = firebase_admin.credentials.Certificate(
+        "src/cousin-secret-santa-database.json"
+    )
+    databaseURL = "https://cousin-secret-santa-default-rtdb.firebaseio.com/"
+    default_app = firebase_admin.initialize_app(
+        cred_object, {"databaseURL": databaseURL}
+    )
+
+
 def writeRecipient(name, recipient):
-	intialize()
-	ref = db.reference("/cousin")
-	ref.child(name).update({'recipient': recipient})
-
-
+    intialize()
+    ref = db.reference("/cousin")
+    ref.child(name).update({"recipient": recipient})
